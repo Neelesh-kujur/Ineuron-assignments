02e75f65a0f8b6fd4a9291278fc42c149099be0c
Neelesh-kujur <110295413+Neelesh-kujur@users.noreply.github.com>
2022-08-19 19:53:33 +0530

    Add files via upload

---
+++ a3q1.java
@@ -0,0 +1,13 @@
+package Assignment_3;
+
+public class a3q1 {
+
+    public static void main(String[] args)
+    {
+        String s="iNeuron";
+        StringBuilder sb=new StringBuilder();
+        sb.append(s);
+        sb.reverse();
+        System.out.println(sb.toString());
+    }
+}
